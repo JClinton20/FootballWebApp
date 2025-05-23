1bba1dda3bf89619cdbcdf7287d4285fdd22eada
JClinton20 <157168898+JClinton20@users.noreply.github.com>
2025-05-23 19:47:56 -0400

    Updated Controllers folder
    Updated Controllers folder

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -1,8 +1,184 @@
-from .user import create_user
+from .user import create_user, create_club, create_player, join_club
 from App.database import db
+import os, csv, requests
+
+API_URL = "https://apiv3.apifootball.com/"
+API_KEY = "93569dc5d3595dcf5599537b3231d52321b5bac97afecec1ff90afbf90c62975"
 
 
 def initialize():
     db.drop_all()
     db.create_all()
-    create_user('bob', 'bobpass')
+    create_user('bob', 'bobpass', 'Bob', 'Marley', 'Jamaica')
+
+    params = {
+        'action': 'get_teams',
+        'league_id': 152,
+        'APIkey': API_KEY
+    }
+    color_codes = [{
+        'name': 'Manchester City',
+        'primary': '#6CABDD',
+        'secondary': '#ffffff',
+        'tertiary': '#1C2C5B'
+    },
+    {
+        'name': 'Liverpool',
+        'primary': '#c8102E',
+        'secondary': '#ffffff',
+        'tertiary': '#00B2A9'
+    },
+    {
+        'name': 'Chelsea',
+        'primary': '#034694',
+        'secondary': '#ffffff',
+        'tertiary': '#dba111'
+    },
+    {
+        'name': 'Manchester United',
+        'primary': '#DA291C',
+        'secondary': '#FBE122',
+        'tertiary': '#000000'
+    },
+    {
+        'name': 'Arsenal',
+        'primary': '#EF0107',
+        'secondary': '#9C824A',
+        'tertiary': '#FFFFFF'
+    },
+    {
+        'name': 'Leicester City',
+        'primary': '#003090',
+        'secondary': '#ffffff',
+        'tertiary': '#fdbe11'
+    },
+    {
+        'name': 'Tottenham Hotspur',
+        'primary': '#132257',
+        'secondary': '#ffffff',
+        'tertiary': '#ffffff'
+    },
+    {
+        'name': 'AFC Bournemouth',
+        'primary': '#DA291C',
+        'secondary': '#000000',
+        'tertiary': '#A89968'
+    },
+    {
+        'name': 'Southampton',
+        'primary': '#d71920',
+        'secondary': '#130c0e',
+        'tertiary': '#FFC20E'
+    },
+    {
+        'name': 'Everton',
+        'primary': '#003399',
+        'secondary': '#ffffff',
+        'tertiary': '#ffffff'
+    },
+    {
+        'name': 'Wolverhampton Wanderers',
+        'primary': '#FDB913',
+        'secondary': '#231F20',
+        'tertiary': '#ffffff'
+    },
+    {
+        'name': 'Brighton & Hove Albion',
+        'primary': '#0057B8',
+        'secondary': '#ffffff',
+        'tertiary': '#ffffff'
+    },
+    {
+        'name': 'West Ham United',
+        'primary': '#7A263A',
+        'secondary': '#1bb1e7',
+        'tertiary': '#F3D459'
+    },
+    {
+        'name': 'Fulham',
+        'primary': '#000000',
+        'secondary': '#ffffff',
+        'tertiary': '#CC0000'
+    },
+    {
+        'name': 'Brentford',
+        'primary': '#D20000',
+        'secondary': '#ffffff',
+        'tertiary': '#FFB400'
+    },
+    {
+        'name': 'Aston Villa',
+        'primary': '#670e36',
+        'secondary': '#95bfe5',
+        'tertiary': '#fee505'
+    },
+    {
+        'name': 'Nottingham Forest',
+        'primary': '#DD0000',
+        'secondary': '#ffffff',
+        'tertiary': '#ffffff'
+    },
+    {
+        'name': 'Newcastle United',
+        'primary': '#241F20',
+        'secondary': '#ffffff',
+        'tertiary': '#F1BE48'
+    },
+    {
+        'name': 'Ipswich Town',
+        'primary': '#3a64a3',
+        'secondary': '#ffffff',
+        'tertiary': '#de2c37'
+    },
+    {
+        'name': 'Crystal Palace',
+        'primary': '#1B458F',
+        'secondary': '#a7a5a6',
+        'tertiary': '#C4122E'
+    }]
+    response = requests.get(API_URL, params=params)
+    if response.status_code == 200:
+        for team in response.json():
+            new_team = create_club(
+                name=team['team_name'],
+                logo=team['team_badge'],
+                country=team['team_country']
+            )
+            for color in color_codes:
+                if new_team.name == color['name']:
+                    new_team.primary_color = color['primary']
+                    new_team.secondary_color = color['secondary']
+                    new_team.tertiary_color = color['tertiary']
+
+            db.session.add(new_team)
+            for player in team.get('players', []):
+                is_captain = False
+                if player['player_is_captain'] == "0" or player['player_is_captain'] == "":
+                    is_captain = False
+                else:
+                    is_captain = True
+
+                player_name = ""
+                if not player['player_complete_name']:
+                    player_name = player['player_name']
+                else:
+                    player_name = player['player_complete_name']        
+                
+                new_player = create_player(
+                    name=player['player_name'],
+                    role=player['player_type'],
+                    country=player['player_country'],
+                    image=player['player_image'],
+                    captain=is_captain,
+                    number=player['player_number'],
+                    age=player['player_age'],
+                    club_id = new_team.id
+                )
+                db.session.add(new_player)
+        join_club(1,1)        
+        db.session.commit()
+        print('Teams and Players Imported!')
+
+        
+
+    
--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,8 +1,9 @@
-from App.models import User
+from App.models import User, Club, Player
 from App.database import db
 
-def create_user(username, password):
-    newuser = User(username=username, password=password)
+
+def create_user(username, password, first_name, last_name, country):
+    newuser = User(username=username, password=password, first_name=first_name, last_name=last_name, country=country)
     db.session.add(newuser)
     db.session.commit()
     return newuser
@@ -30,4 +31,78 @@ def update_user(id, username):
         db.session.add(user)
         return db.session.commit()
     return None
+
+#Club Functions
+
+def create_club(name, logo, country):
+    newClub = Club(name=name, logo=logo, country=country)
+    db.session.add(newClub)
+    db.session.commit()
+    return newClub
+
+def get_club(id):
+    return Club.query.get(id)
+
+def get_all_clubs():
+    return Club.query.all()
+
+def search_clubs(page):
+    matching_club = Club.query
+    return matching_club.paginate(page=page, per_page=5)
+
+def current_club(id):
+    current_club = Club.query.get(id)
+    return current_club
+
+def next_club(id):
+    next_club = Club.query.filter(Club.id > id).order_by(Club.id.asc()).first()
+    return next_club
+
+def prev_club(id):
+    prev_club = Club.query.filter(Club.id < id).order_by(Club.id.desc()).first()
+    return prev_club
+
+#User Functions
+
+def join_club(user_id, club_id):
+    user = get_user(user_id)
+    club = get_club(club_id)
+
+    club.manager = user
+    db.session.commit()
+
+def leave_club(user_id, club_id):
+    user = get_user(user_id)
+    club = get_club(club_id)
+
+    if club.manager == user and user.club == club:
+        club.manager = None
+        user.club = None
+        db.session.commit()
+    return None
+
+
+#Player Functions
+def create_player(name, role, country, image, captain, number, age, club_id):
+    new_player = Player(name=name,role=role,country=country,image=image,captain=captain,number=number,age=age, club_id=club_id)
+    new_player.club = get_club(club_id)
+
+    db.session.add(new_player)
+    db.session.commit()
+    # player_id = new_player.id
+    # player_name = new_player.name
+    # club = get_club(club_id)
+    # club_name = club.name
+    # new_club_player = ClubPlayer(club_id=club_id, club_name=club_name, player_id=player_id, player_name=player_name)
+    # db.session.add(new_club_player)
+    # db.session.commit()
+    return new_player
     
+def get_all_players():
+    return Player.query.all()
+
+def get_club_players(club_id):
+    players = Player.query.filter_by(club_id=club_id).all()
+    return players
+    
+
